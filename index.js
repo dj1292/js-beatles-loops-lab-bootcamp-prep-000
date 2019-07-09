1e0188fc86d6f12e258ddeb879343220d673d2e3
Dan Reeves <djreeves1292@gmail.com>
2019-07-09 03:12:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// add solution here
+var new_arr = []
+var i = 0
+function theBeatlesPlay(arr1,arr2) {
+  for (i = 0; i < arr1.length; i++) {
+    new_arr.push(`${arr1[i]} plays ${arr2[i]}`)
+  }
+   return new_arr
+}
+
+
+function johnLennonFacts(arr) {
+  var newArr = []
+var i = 0
+  while (i < arr.length) {
+   newArr << `${arr}!!!`
+   i += 1
+  }
+  return newArr 
+}
+
+
